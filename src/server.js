9f885480f2f67b7d6e0d1e65b9cf91bbe9c3c2ab
andres carrero <andres.carrero3107@gmail.com>
2020-11-27 17:07:31 -0500

    proteccion db

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlbd;
-if (process.env.NODE_ENV === 'dev') {
-    urlbd = 'mongodb://localhost:27017/apdPrueba';
-} else {
-    urlbd = 'mongodb+srv://andres:andres.com@adp.eilwf.mongodb.net/adp'
-}
+//if (process.env.NODE_ENV === 'dev') {
+// urlbd = 'mongodb://localhost:27017/apdPrueba';
+//} else {
+urlbd = process.env.MONGO_DB
+    //}
 process.env.URLM = urlbd
